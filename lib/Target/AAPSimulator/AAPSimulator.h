44a2a68537e7f580fc66221de87cafced2c733e6
Simon Cook <simon.cook@embecosm.com>
2015-11-02 14:23:08 +0000

    AAP Simulator: Add missing header files from AAPSimulator

--- lib/Target/AAPSimulator/AAPSimulator.h
+++ lib/Target/AAPSimulator/AAPSimulator.h
@@ -14,7 +14,12 @@
 #ifndef LLVM_LIB_TARGET_AAPSIMULATOR_AAPSIMULATOR_H
 #define LLVM_LIB_TARGET_AAPSIMULATOR_AAPSIMULATOR_H
 
+#include "llvm/MC/MCDisassembler.h"
 #include "llvm/MC/MCInst.h"
+#include "llvm/MC/MCInstrInfo.h"
+#include "llvm/MC/MCRegisterInfo.h"
+#include "llvm/MC/MCSubtargetInfo.h"
+#include "llvm/Support/TargetRegistry.h"
 #include "AAPSimState.h"
 
 namespace AAPSim {
