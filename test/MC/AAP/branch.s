5a3d6a8c3122dcc7b70b8334f9d6bb296536caaf
Edward Jones <ed.jones@embecosm.com>
2016-01-11 17:19:49 +0000

    AAP: Convert branch test to test for correct encoding
    Before, these tests simply checked that the instructions were parsed
    correctly, and that the shortest possible instruction was selected.
    
    The remaining MC tests need to be converted to check encodings.

--- test/MC/AAP/branch.s
+++ test/MC/AAP/branch.s
@@ -1,22 +1,22 @@
-; RUN: llvm-mc -triple=aap -show-inst %s | FileCheck %s
+; RUN: llvm-mc -triple=aap -show-encoding %s | FileCheck %s
 
 ; Basic tests to check that we always pick the correct (and shorest)
 ; branch instructions
 ; Short branch instructions are never assembled
 
-beq    511, $r0,  $r1   ;CHECK: {{BEQ_$}}
-bne   -512, $r7,  $r0   ;CHECK: {{BNE_$}}
-blts    15, $r13, $r61  ;CHECK: {{BLTS_$}}
-bles    32, $r9,  $r8   ;CHECK: {{BLES_$}}
-bltu   186, $r11, $r12  ;CHECK: {{BLTU_$}}
-bleu    -1, $r17, $r19  ;CHECK: {{BLEU_$}}
+beq    511, $r0,  $r1   ;CHECK: encoding: [0xc1,0xc5,0xc0,0x0f]
+bne   -512, $r7,  $r0   ;CHECK: encoding: [0x38,0xc6,0x00,0x10]
+blts    15, $r13, $r61  ;CHECK: encoding: [0xed,0xc9,0x4f,0x00]
+bles    32, $r9,  $r8   ;CHECK: encoding: [0x08,0xca,0x09,0x01]
+bltu   186, $r11, $r12  ;CHECK: encoding: [0x9c,0xcc,0xc9,0x05]
+bleu    -1, $r17, $r19  ;CHECK: encoding: [0xcb,0xcf,0xd2,0x1f]
 
-bra      123    ;CHECK: {{BRA$}}
-bra    -6810    ;CHECK: {{BRA$}}
-bra  -131072    ;CHECK: {{BRA$}}
-bra   131071    ;CHECK: {{BRA$}}
+bra      123    ;CHECK: encoding: [0x7b,0xc0,0x00,0x00]
+bra    -6810    ;CHECK: encoding: [0x66,0xc1,0xf2,0x1f]
+bra  -131072    ;CHECK: encoding: [0x00,0xc0,0x00,0x1f]
+bra   131071    ;CHECK: encoding: [0xff,0xc1,0xff,0x00]
 
-bal      481, $r14    ;CHECK: {{BAL$}}
-bal    -1892, $r58    ;CHECK: {{BAL$}}
-bal    32767, $r0     ;CHECK: {{BAL$}}
-bal   -32768, $r15    ;CHECK: {{BAL$}}
+bal      481, $r14    ;CHECK: encoding: [0x0e,0xc3,0x39,0x00]
+bal    -1892, $r58    ;CHECK: encoding: [0xe2,0xc2,0x17,0x1f]
+bal    32767, $r0     ;CHECK: encoding: [0xf8,0xc3,0xf8,0x0f]
+bal   -32768, $r15    ;CHECK: encoding: [0x07,0xc2,0x01,0x10]
