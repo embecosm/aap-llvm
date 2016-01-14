2b143216cd1e91ef37bbe677b0ed3c822576f050
Edward Jones <ed.jones@embecosm.com>
2016-01-14 14:47:13 +0000

    AAP: Convert noop test to check for encodings

--- test/MC/AAP/noop.s
+++ test/MC/AAP/noop.s
@@ -1,10 +1,10 @@
-; RUN: llvm-mc -triple=aap -show-inst %s | FileCheck %s
+; RUN: llvm-mc -triple=aap -show-encoding %s | FileCheck %s
 
 ; Basic tests to check that we always pick the correct (and shortest)
 ; noop instruction.
 
-nop   $r0,  5   ;CHECK: {{NOP_short}}
-nop   $r7,  63  ;CHECK: {{NOP_short}}
-nop   $r8,  53  ;CHECK: {{NOP$}}
-nop   $r4,  64  ;CHECK: {{NOP$}}
-nop   $r63, a   ;CHECK: {{NOP$}}
+nop   $r0,  5   ;CHECK: encoding: [0x05,0x00]
+nop   $r7,  63  ;CHECK: encoding: [0xff,0x01]
+nop   $r8,  53  ;CHECK: encoding: [0x35,0x80,0x40,0x00]
+nop   $r4,  64  ;CHECK: encoding: [0x00,0x81,0x01,0x00]
+nop   $r63, a   ;CHECK: encoding: [0b11AAAAAA,0x81,0xc0,0x01]
