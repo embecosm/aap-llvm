d7a95e54a2898174e9d1aa33d0d58be906467c19
Edward Jones <ed.jones@embecosm.com>
2016-03-29 14:17:35 +0100

    AAP: Removed methods with identical default impls
    isLoadFromStackSlot and isStoreToStackSlot had the same implementation
    as the default

--- lib/Target/AAP/AAPInstrInfo.cpp
+++ lib/Target/AAP/AAPInstrInfo.cpp
@@ -37,25 +37,6 @@ void AAPInstrInfo::anchor() {}
 AAPInstrInfo::AAPInstrInfo(AAPSubtarget &STI)
     : AAPGenInstrInfo(AAP::ADJCALLSTACKDOWN, AAP::ADJCALLSTACKUP), TRI() {}
 
-/// isLoadFromStackSlot - If the specified machine instruction is a direct
-/// load from a stack slot, return the virtual or physical register number of
-/// the destination along with the FrameIndex of the loaded stack slot.  If
-/// not, return 0.  This predicate must return 0 if the instruction has
-/// any side effects other than loading from the stack slot.
-unsigned AAPInstrInfo::isLoadFromStackSlot(const MachineInstr *MI,
-                                           int &FrameIndex) const {
-  return 0;
-}
-
-/// isStoreToStackSlot - If the specified machine instruction is a direct
-/// store to a stack slot, return the virtual or physical register number of
-/// the source reg along with the FrameIndex of the loaded stack slot.  If
-/// not, return 0.  This predicate must return 0 if the instruction has
-/// any side effects other than storing to the stack slot.
-unsigned AAPInstrInfo::isStoreToStackSlot(const MachineInstr *MI,
-                                          int &FrameIndex) const {
-  return 0;
-}
 
 //===----------------------------------------------------------------------===//
 // Branch Analysis
--- lib/Target/AAP/AAPInstrInfo.h
+++ lib/Target/AAP/AAPInstrInfo.h
@@ -33,17 +33,6 @@ public:
 
   const TargetRegisterInfo &getRegisterInfo() const { return TRI; }
 
-  unsigned isLoadFromStackSlot(const MachineInstr *MI,
-                               int &FrameIndex) const override;
-
-  /// isStoreToStackSlot - If the specified machine instruction is a direct
-  /// store to a stack slot, return the virtual or physical register number of
-  /// the source reg along with the FrameIndex of the loaded stack slot.  If
-  /// not, return 0.  This predicate must return 0 if the instruction has
-  /// any side effects other than storing to the stack slot.
-  unsigned isStoreToStackSlot(const MachineInstr *MI,
-                              int &FrameIndex) const override;
-
   bool AnalyzeBranch(MachineBasicBlock &MBB, MachineBasicBlock *&TBB,
                      MachineBasicBlock *&FBB,
                      SmallVectorImpl<MachineOperand> &Cond,
