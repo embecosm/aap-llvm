ad567aefed592f0456c9078e4e4e290fd43cf8fa
Edward Jones <ed.jones@embecosm.com>
2016-08-18 09:14:21 +0100

    AAP: Add missing include of MCFixup.h

--- lib/Target/AAP/MCTargetDesc/AAPMCCodeEmitter.h
+++ lib/Target/AAP/MCTargetDesc/AAPMCCodeEmitter.h
@@ -16,6 +16,7 @@
 
 #include "llvm/MC/MCCodeEmitter.h"
 #include "llvm/MC/MCExpr.h"
+#include "llvm/MC/MCFixup.h"
 #include "llvm/MC/MCInst.h"
 #include "llvm/MC/MCInstrInfo.h"
 #include "llvm/MC/MCRegisterInfo.h"
