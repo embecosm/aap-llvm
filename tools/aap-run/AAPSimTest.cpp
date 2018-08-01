9b647a6ea6f8c2a75542186481ec20e11d6b8d3e
Lewis Revill <lewis.revill@embecosm.com>
2018-08-01 17:10:38 +0100

    AAP: Make sure the simulator indicates failure correctly
    The AAP simulator should return a non-zero value for all the final
    statuses which do not indicate normal exit conditions. Previously the
    optimistic return codes were causing tests to report success when they
    should have failed.

--- tools/aap-run/AAPSimTest.cpp
+++ tools/aap-run/AAPSimTest.cpp
@@ -136,7 +136,8 @@ int main(int argc, char **argv) {
 
   // Deal with the final simulator status
   switch (status) {
-    default: break;
+    case SimStatus::SIM_OK:
+      llvm_unreachable("Not a valid final status");
     case SimStatus::SIM_INVALID_INSN:
       outs() << " *** Attempted to decode invalid instruction ***\n";
       break;
@@ -148,17 +149,17 @@ int main(int argc, char **argv) {
       break;
     case SimStatus::SIM_EXCEPT_MEM:
       outs() << " *** Invalid memory trap ***\n";
-      return 1;
+      break;
     case SimStatus::SIM_EXCEPT_REG:
       outs() << " *** Invalid register trap ***\n";
-      return 1;
+      break;
     case SimStatus::SIM_TIMEOUT:
       outs() << " *** Simulator timeout ***\n";
-      return 1;
+      break;
     case SimStatus::SIM_QUIT:
       outs() << " *** EXIT CODE " << Sim.getState().getExitCode() << " ***\n";
       return Sim.getState().getExitCode();
   }
 
-  return 0;
+  return 1;
 }
